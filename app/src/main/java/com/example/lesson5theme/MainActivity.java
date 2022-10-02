718f47114ea65762fd02135e80c07995b96f38f2
ivanh <rik.chanel@gmail.com>
2022-10-02 18:24:30 +0300

    dropdown

--- app/src/main/java/com/example/lesson5theme/MainActivity.java
+++ app/src/main/java/com/example/lesson5theme/MainActivity.java
@@ -3,16 +3,45 @@ package com.example.lesson5theme;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+     String [] countries = { "first", "second", "third"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TextView selection = findViewById(R.id.text);
+        Spinner spinner = findViewById(R.id.spinner);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,countries);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
 
 
+        AdapterView.OnItemSelectedListener itemSelectedListener = new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String item = (String) parent.getItemAtPosition(position);
+                selection.setText(item);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        };
+
+        spinner.setOnItemSelectedListener(itemSelectedListener);
 
     }
 }
